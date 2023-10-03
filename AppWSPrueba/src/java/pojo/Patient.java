d52d00191a654bd8b79c11f9241b3f795b51125f
Andre-HC11 <76708342+Andre-HC11@users.noreply.github.com>
2023-10-02 21:40:30 -0600

    Update Patient.java

--- AppWSPrueba/src/java/pojo/Patient.java
+++ AppWSPrueba/src/java/pojo/Patient.java
@@ -7,31 +7,49 @@ package pojo;
  */
 public class Patient {
 
-    private Integer idUsuario;
+    private Integer idPaciente;
     private String nombre;
     private String apellidoPaterno;
     private String apellidoMaterno;
-    private String correo;
+    private String fechaNacimiento;
+    private String sexo;
+    private float peso;
+    private float estatura;
+    private int tallaInicial;
+    private String email;
+    private String telefono;
     private String password;
+    private byte[] fotografia;
+    private Integer idDomicilio;
+    private Integer idMedico;
 
     public Patient() {
     }
 
-    public Patient(Integer idUsuario, String nombre, String apellidoPaterno, String apellidoMaterno, String correo, String password) {
-        this.idUsuario = idUsuario;
+    public Patient(Integer idPaciente, String nombre, String apellidoPaterno, String apellidoMaterno, String fechaNacimiento, String sexo, float peso, float estatura, int tallaInicial, String email, String telefono, String password, byte[] fotografia, Integer idDomicilio, Integer idMedico) {
+        this.idPaciente = idPaciente;
         this.nombre = nombre;
         this.apellidoPaterno = apellidoPaterno;
         this.apellidoMaterno = apellidoMaterno;
-        this.correo = correo;
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.estatura = estatura;
+        this.tallaInicial = tallaInicial;
+        this.email = email;
+        this.telefono = telefono;
         this.password = password;
+        this.fotografia = fotografia;
+        this.idDomicilio = idDomicilio;
+        this.idMedico = idMedico;
     }
 
-    public Integer getIdUsuario() {
-        return idUsuario;
+    public Integer getIdPaciente() {
+        return idPaciente;
     }
 
-    public void setIdUsuario(Integer idUsuario) {
-        this.idUsuario = idUsuario;
+    public void setIdPaciente(Integer idPaciente) {
+        this.idPaciente = idPaciente;
     }
 
     public String getNombre() {
@@ -58,12 +76,60 @@ public class Patient {
         this.apellidoMaterno = apellidoMaterno;
     }
 
-    public String getCorreo() {
-        return correo;
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
     }
 
-    public void setCorreo(String correo) {
-        this.correo = correo;
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getEstatura() {
+        return estatura;
+    }
+
+    public void setEstatura(float estatura) {
+        this.estatura = estatura;
+    }
+
+    public int getTallaInicial() {
+        return tallaInicial;
+    }
+
+    public void setTallaInicial(int tallaInicial) {
+        this.tallaInicial = tallaInicial;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
     }
 
     public String getPassword() {
@@ -72,5 +138,29 @@ public class Patient {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public byte[] getFotografia() {
+        return fotografia;
+    }
+
+    public void setFotografia(byte[] fotografia) {
+        this.fotografia = fotografia;
+    }
+
+    public Integer getIdDomicilio() {
+        return idDomicilio;
+    }
+
+    public void setIdDomicilio(Integer idDomicilio) {
+        this.idDomicilio = idDomicilio;
+    }
+
+    public Integer getIdMedico() {
+        return idMedico;
+    }
+
+    public void setIdMedico(Integer idMedico) {
+        this.idMedico = idMedico;
     }
 }
