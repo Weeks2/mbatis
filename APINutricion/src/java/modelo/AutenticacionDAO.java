61ecbf97ece364779f8313fbaee6dd6a6d0df608
Andre-HC11 <76708342+Andre-HC11@users.noreply.github.com>
2023-10-14 10:02:21 -0600

    clean

--- APINutricion/src/java/modelo/AutenticacionDAO.java
+++ APINutricion/src/java/modelo/AutenticacionDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import java.util.HashMap;
@@ -11,10 +6,6 @@ import modelo.pojo.RespuestaLoginEscritorio;
 import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
-/**
- *
- * @author andre
- */
 public class AutenticacionDAO {
 
     public static RespuestaLoginEscritorio verificarSesionEscritorio(String numeroDePersonal, String password) {
--- APINutricion/src/java/modelo/PacienteDAO.java
+++ APINutricion/src/java/modelo/PacienteDAO.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo;
 
 import java.util.HashMap;
@@ -13,10 +8,6 @@ import modelo.pojo.Paciente;
 import mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 
-/**
- *
- * @author andre
- */
 public class PacienteDAO {
     public List<Paciente> obtenerPacientePorIdMedico(Integer idMedico){
         List<Paciente> paciente = null;
