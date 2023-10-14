5244944508ea8328db34a7026b308c037dda2b77
Andre-HC11 <76708342+Andre-HC11@users.noreply.github.com>
2023-10-14 10:06:48 -0600

    clean

--- APINutricion/src/java/modelo/pojo/Paciente.java
+++ APINutricion/src/java/modelo/pojo/Paciente.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package modelo.pojo;
 
-/**
- *
- * @author andre
- */
 public class Paciente {
     private Integer idPaciente;
     private String nombre;
