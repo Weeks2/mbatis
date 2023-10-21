57bbd372e87bd48328b32739c898065be17bf1b7
Andre-HC11 <76708342+Andre-HC11@users.noreply.github.com>
2023-10-21 11:29:14 -0600

    Update JsonUtility.java

--- ClienteEscritorioNutricion/src/clienteescritorionutricion/modelo/JsonUtility.java
+++ ClienteEscritorioNutricion/src/clienteescritorionutricion/modelo/JsonUtility.java
@@ -1,30 +1,22 @@
 package clienteescritorionutricion.modelo;
+
 import clienteescritorionutricion.modelo.pojo.Paciente;
 import com.google.gson.Gson;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JsonUtility {
-    
- public static String createJson(Paciente paciente) {
-    StringBuilder jsonBuilder = new StringBuilder();
-    jsonBuilder.append("{")
-               .append(addChild("nombre", paciente.getNombre(), true))
-               .append(addChild("apellidoPaterno", paciente.getApellidoPaterno(),true))
-               .append(addChild("apellidoMaterno", paciente.getApellidoMaterno(), false))
-               .append("}");
 
-    return jsonBuilder.toString();
-}
+    public static String createJson(Paciente paciente) {
+        StringBuilder jsonBuilder = new StringBuilder();
+        jsonBuilder.append("{")
+                .append(addChild("nombre", paciente.getNombre(), true))
+                .append(addChild("apellidoPaterno", paciente.getApellidoPaterno(), true))
+                .append(addChild("apellidoMaterno", paciente.getApellidoMaterno(), false))
+                .append("}");
 
-private static String addChild(String key, String val, boolean comma) {
-    return String.format("\"%s\":\"%s\"%s", key, val, comma ? "\",":"");
-}
+        return jsonBuilder.toString();
+    }
 
-private static String toJson() {
-    Gson gson = new Gson();
-    ObjectMapper mapper = new ObjectMapper();
-    return gson.toJson(new Paciente());
-    
-    
- }
+    private static String addChild(String key, String val, boolean comma) {
+        return String.format("\"%s\":\"%s\"%s", key, val, comma ? "\"," : "");
+    }
 }
