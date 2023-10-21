fa92f97f0ac1872eb90189940f643e44e5bcb538
Andre-HC11 <76708342+Andre-HC11@users.noreply.github.com>
2023-10-21 12:30:13 -0600

    add update paciente

--- ClienteEscritorioNutricion/src/clienteescritorionutricion/FXMLEditarPacienteController.java
+++ ClienteEscritorioNutricion/src/clienteescritorionutricion/FXMLEditarPacienteController.java
@@ -5,15 +5,21 @@
  */
 package clienteescritorionutricion;
 
+import clienteescritorionutricion.modelo.dao.PacienteDAO;
+import clienteescritorionutricion.modelo.pojo.Mensaje;
 import clienteescritorionutricion.modelo.pojo.Paciente;
+import clienteescritorionutricion.utils.Utilidades;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -56,5 +62,44 @@ public class FXMLEditarPacienteController implements Initializable {
         tfEmail.setDisable(true);
         tfEmail.setText(infoPaciente.getEmail());
         tfTelefono.setText(infoPaciente.getTelefono());
+    }
+    
+    private void btnRegistrarPaciente(ActionEvent event) {
+        Paciente pacienteNuevo = new Paciente();
+        pacienteNuevo.setNombre(tfNombre.getText());
+        pacienteNuevo.setApellidoPaterno(tfApellidoPaterno.getText());
+        pacienteNuevo.setApellidoMaterno(tfApellidoMaterno.getText());
+        pacienteNuevo.setFechaNacimiento(dpFechaNacimiento.getValue().toString());
+        pacienteNuevo.setSexo("");
+        pacienteNuevo.setPeso(1.5F);
+        pacienteNuevo.setEstatura(0.1F);
+        pacienteNuevo.setTallaInicial(50);
+        pacienteNuevo.setEmail(tfEmail.getText());
+        pacienteNuevo.setTelefono(tfTelefono.getText());
+        pacienteNuevo.setPassword("");
+        pacienteNuevo.setIdMedico(1);
+        
+        registrarPaciente(pacienteNuevo);
+        
+    }
+    
+    private void verificarPaciente (){
+        
+    }
+    
+    private void registrarPaciente (Paciente pacienteNuevo){
+        Mensaje msj = PacienteDAO.registrarPaciente(pacienteNuevo);
+        
+        if (!msj.isError()) {
+            Utilidades.mostrarAlertaSimple("Paciente registrado", msj.getMensaje(), Alert.AlertType.INFORMATION);
+            cerrarVentana();
+        } else {
+            Utilidades.mostrarAlertaSimple("Error al registrar", msj.getMensaje(), Alert.AlertType.ERROR);
+        }
+    }
+    
+    private void cerrarVentana(){
+        Stage stage = (Stage) tfNombre.getScene().getWindow();
+        stage.close();
     }
 }
